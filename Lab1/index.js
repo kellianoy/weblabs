06bf9c16cc2978c0929a52cbb33d9712896c6339
Kellian <kellian.cottart@edu.ece.fr>
2021-09-29 18:42:19 +0200

    LAB 1 DONE

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,40 +1,8 @@
 // Import Node html module
 const http = require('http')
-// Import Node url module
-const url = require('url')
-//Import Node querystring module
-const qs = require('querystring')
+//Import handles module
+const handles = require('./handles')
 
-// Define a string constant concatenating strings
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <h1>Welcome to the new technology</h1>' +
-'        <p>Life if short but it would be cool to die earlier though</p>' +
-'    </body>' +
-'</html>'
-
-const serverHandle = function (req, res) {
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
-  
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
-  
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(content);  
-  res.end();
-}
-
-const server = http.createServer(serverHandle);
+const server = http.createServer(handles.serverHandle);
 server.listen(8080)
 
