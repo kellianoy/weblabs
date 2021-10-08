e2049d9d07ea00726ea2bfffab77d7f419b6368f
KekeLinux <kellian.cottart@edu.ece.fr>
2021-10-08 08:44:12 +0200

    week3

--- Lab1/index.js
+++
@@ -1,8 +0,0 @@
-// Import Node html module
-const http = require('http')
-//Import handles module
-const handles = require('./handles')
-
-const server = http.createServer(handles.serverHandle);
-server.listen(8080)
-
