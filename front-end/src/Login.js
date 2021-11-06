2deb09fbafd86a67d198fc1003a4b09e7871ee0c
KekeLinux <kellian.cottart@edu.ece.fr>
2021-11-06 16:52:33 +0100

    chore: finish step 8 / 9 / 10

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -1,9 +1,26 @@
 /** @jsxImportSource @emotion/react */
 // Layout
 import { useTheme } from '@mui/styles';
-
-import {Button, TextField} from '@mui/material';
+import { Button } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
+
+import Cookies from 'universal-cookie';
+import crypto from 'crypto';
+import queryString from 'query-string';
+import axios from 'axios';
+import qs from 'qs'
+
+//Encrypting data for challenge
+function base64URLEncode(str) {
+  return str.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+}
+
+function sha256(buffer) {
+  return crypto.createHash('sha256').update(buffer).digest();
+}
+
+//starting cookies
+const cookies = new Cookies();
 
 const useStyles = (theme) => ({
   root: {
@@ -27,29 +44,81 @@ const useStyles = (theme) => ({
   },
 })
 
-export default function Login({
-  onUser
+export default function Login
+({ 
+  onUser 
 }) {
   const styles = useStyles(useTheme())
-  return (
-    <div css={styles.root}>
-      <div>
-        <h1 css={{marginLeft: '10px'}}>Please, log in the application</h1>
-        <fieldset>
-          <TextField id="username" label="Username" color="secondary" variant="standard" />
-        </fieldset>
-        <fieldset>
-          <TextField id="password" label="Password" color="secondary" variant="standard" type="password"/>
-        </fieldset>
-        <fieldset>
-          <Button variant="contained" color="primary" onClick={ (e) => {
-              e.stopPropagation()
-              onUser({username: 'david'})
-            }}>
-            Log In <LoginIcon/>
-          </Button>
-        </fieldset>
-      </div>
-    </div>
-  );
+  
+  //let's check if there are params
+  const url = window.location.search;
+  const params = queryString.parse(url);
+
+  //If there is a query code
+  if (params.code)
+  {
+
+
+    axios.post("http://127.0.0.1:5556/dex/token", qs.stringify(
+      {
+        grant_type: 'authorization_code',
+        client_id: "example-app",
+        redirect_uri: "http://localhost:3000/callback",
+        code_verifier: cookies.get('codeVerifier'),
+        code: params.code
+      }
+    )).then(function(response){
+      console.log(response);
+    }); 
+    return null;
+  }
+  else {
+      //Check if the user is logged in or not, if so, let's enter the app
+    if (cookies.get("username")) {
+      onUser( { username: cookies.get('username') })
+      return null;
+    }
+    else { //if he is not logged, let's register
+      return (
+        <div css={styles.root}>
+          <div>
+            <h1 css={{ marginLeft: '10px' }}>Please, log in the application</h1>
+            <fieldset>
+              <Button variant="contained" color="primary" onClick={(e) => {
+                e.stopPropagation()
+      
+                //First, let's build our url
+
+                //let's generate a code verifier
+                const code_verifier = base64URLEncode(crypto.randomBytes(32))
+
+                //let's save it in the cookies
+                cookies.set('codeVerifier', code_verifier, { path: '/' });
+                console.log(cookies.get('codeVerifier'));
+
+                //let's create a code challenge for this code
+                const code_challenge = base64URLEncode(sha256(code_verifier))
+
+                //we can now create our url
+                var url = ["http://127.0.0.1:5556/dex/auth" 
+                    + "?", "client_id=example-app"  
+                    + "&", "scope=openid email offline_access"
+                    + "&", "response_type=code&", "redirect_uri=http://localhost:3000/callback"
+                    + "&", "code_challenge=" + code_challenge 
+                    + "&", "code_challenge_method=S256"].join('');
+                
+                //and redirect the user to it
+                console.log(url)
+                window.location.replace(url)
+                
+              }}>
+                Log In <LoginIcon />
+              </Button>
+            </fieldset>
+          </div>
+        </div>
+  
+      );
+    }
+  }
 }
